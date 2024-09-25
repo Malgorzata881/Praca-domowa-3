c8d5eacfbe825292a165bdc4a2537d76eaaecdcd
Malgorzata881 <m.michalczuk7@o2.pl>
2024-09-25 12:59:50 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+console.log()
+
+const numbers = [5, 7, 19, 34, 21, 11, 0, 100];
+
+for (const number of numbers) {
+    if (number > 5) {
+        console.log(number);
+    }
+}
